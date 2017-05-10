9305eb732f10aed5213672d8a93b501d00c54cce
Noer <noerpaanakker@hotmail.com>
2017-05-10 17:27:46 +0200

    created users.json

--- app.js
+++ app.js
@@ -1,2 +1,11 @@
 const express = require('express');
 const app = express();
+
+// respond with 'This is working!' when a GET request is made
+app.get('/', (request, response) => {
+	response.send('This is working!');
+})
+
+app.post('/path/to/file', {param1: 'value1'}, function(data, textStatus, xhr) {
+	/*optional stuff to do after success */
+});
