3e44624a4804dc705b136dec0c21dc15af0edcb1
Noer <noerpaanakker@hotmail.com>
2017-05-13 17:21:45 +0200

    created 4 more routes

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const app = require('express')();
 const fs = require('fs');
+const bodyparser = require('body-parser');
 
 app.set('views', 'views');
 app.set('view engine', 'pug');
@@ -18,8 +19,21 @@ app.get('/', (request, response) => {
 });
 
 app.get('/search', (request, response) => {
-	
-})
+	response.render('search');
+	response.send('Everything OK!');
+});
+
+app.post('/users', (request, response) => {
+	response.render('users');
+});
+
+app.get('/createuser', (request, response) => {
+	response.render('createuser');
+});
+
+app.post('/adduser', (request, response) => {
+	response.render('adduser');
+});
 
 const listener = app.listen(3000, () => {
 	console.log('The server has started at port:', listener.address().port)
