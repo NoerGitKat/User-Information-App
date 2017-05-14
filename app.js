18f1754b6b4afc68d0a06384ec259818a157e12a
Noer <noerpaanakker@hotmail.com>
2017-05-14 19:45:35 +0200

    finished route 4 and 5

--- app.js
+++ app.js
@@ -7,38 +7,66 @@ app.use('/', bodyParser);
 app.set('views', 'views');
 app.set('view engine', 'pug');
 
-app.get('/', (request, response) => {
-	fs.readFile('./users.json', 'utf-8', (err, data) => {
+// loading file that saves user information
+
+fs.readFile('./users.json', 'utf-8', (err, data) => {
 		if (err) {
 			console.log(`Beep boop, error occurred: ${err}`);
 			throw err;
 		}
 		var parse = JSON.parse(data);
-		response.render('index', { 
-			users: parse
+
+// ROUTE 1: show all users 
+
+	app.get('/', (request, response) => {
+			response.render('index', { 
+				users: parse
+			});
 		});
+
+// ROUTE 2: search bar to look for matches in user database
+
+	app.get('/search', (request, response) => {
+		response.render('search');
 	});
-});
 
-app.get('/search', (request, response) => {
-	response.render('search');
-});
+// ROUTE 3: display matching users through comparison
 
-app.post('/search', (request, response) => {
-	console.log(request.body.name);
-	if (request.body.name === 'doge' || 'wow'){
-		response.send('User found!');	
-	} else {
-		response.send('Not a user.')
-	}
-});
+	app.post('/search', (request, response) => {
+		console.log(request.body.name);
 
-app.get('/createuser', (request, response) => {
-	response.render('createuser');
-});
+			if (request.body.name == parse.firstname || parse.lastname || parse.firstname + parse.lastname){
+				response.send('User found!');	
+			} else {
+				response.send('Not a user.')
+			}
+		});
 
-app.put('/adduser', (request, response) => {
-	response.render('adduser');
+// ROUTE 4: created 3 forms for user input to create account
+
+	app.get('/createaccount', (request, response) => {
+		response.render('createaccount');
+	});
+
+//	ROUTE 5: convert user input into object and push into json file
+
+	app.post('/createaccount', (request, response) => {
+		console.log(request.body.firstname + " " + request.body.lastname + " " + request.body.email)
+		var newuser = { firstname: request.body.firstname, lastname: request.body.lastname, email: request.body.email };
+		
+		parse.push(newuser);
+		var newjson = JSON.stringify(parse);
+
+		fs.writeFile('./users.json', newjson, 'utf-8', (err) => {
+			if (err) {
+				console.log(`Beep boop, error occurred: ${err}`);
+			throw err;
+			} else {
+				console.log('Database updated!');
+			}
+		});
+		response.redirect('/');
+	});
 });
 
 const listener = app.listen(3000, () => {
